dbd2a35a05247b3cdaa4b4a4b214460fdc4dae41
SONU KOL <108781856+SONUKOL1@users.noreply.github.com>
2023-03-18 19:26:53 +0530

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,23 @@
+import machine
+import time
+
+# Set up the LED pin
+led_pin = machine.Pin(2, machine.Pin.OUT)
+
+# Set up the serial communication
+uart = machine.UART(0, baudrate=115200)
+
+# Loop forever
+while True:
+    # Turn the LED on
+    led_pin.on()
+    # Print a message to the serial monitor
+    uart.write('LED ON\n'.encode())
+    # Wait for 1 second
+    time.sleep(1)
+    # Turn the LED off
+    led_pin.off()
+    # Print a message to the serial monitor
+    uart.write('LED OFF_ota_check_boot_py\n'.encode())
+    # Wait for 1 second
+    time.sleep(1)
